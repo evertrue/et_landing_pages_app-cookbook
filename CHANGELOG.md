3720d34f8c5efe7a172008b8c0182f4161bbc53e
Jeff Byrnes <jeff@evertrue.com>
2014-02-07 16:53:47 -0500

    Bump to v1.0.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 1.0.1
+
+* Name change silliness
+
 ## 1.0.0
 
 * Initial production release
--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'devops@evertrue.com'
 license          'All rights reserved'
 description      'Installs/Configures et_landing_pages_app'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.0'
+version          '1.0.1'
 
 depends 'et_rails_app', '~> 3.0.2'
