0581003aca24fc3cecd162a61bf18ed815aec61d
Eric Herot <eric.github@herot.com>
2014-02-10 14:26:32 -0500

    Bump et_rails_app 3.0.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 1.0.2
+
+* Bump et_rails_app 3.0.3
+
 ## 1.0.1
 
 * Name change silliness
--- attributes/default.rb
+++ attributes/default.rb
@@ -2,6 +2,11 @@ set['nginx']['default_site_enabled'] = false
 set['nginx']['socketproxy']['default_app'] = 'landing_pages'
 set['nginx']['socketproxy']['apps'] = {
   'landing_pages' => {
+    'remote_assets' => {
+      'bucket' => "landing-pages-#{node.chef_environment}.evertrue.com",
+      'path' => '/',
+      'account' => "LandingPages-#{node.chef_environment}"
+    },
     'prepend_slash' => false,
     'context_name' => '',
     'subdir' => 'current',
