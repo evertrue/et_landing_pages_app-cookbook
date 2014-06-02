66791cd7aae6a194bbd3350c56982b878b96b911
Eric Herot <eric.github@herot.com>
2014-06-02 15:00:17 -0400

    Bump et_rails_app v5.0.4

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,6 @@ description      'Installs/Configures et_landing_pages_app'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.9'
 
-depends 'et_rails_app', '= 5.0.3'
+depends 'et_rails_app', '= 5.0.4'
 depends 'et_users',     '>= 1.4.1'
 depends 'apt'
