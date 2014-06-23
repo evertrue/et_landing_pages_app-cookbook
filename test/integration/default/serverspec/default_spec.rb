47e2f52362a08387415cd24d6b91a3e4833ca81b
Jeff Byrnes <jeff@evertrue.com>
2014-06-23 15:48:55 -0400

    Convert Serverspecs to RSpec 3.0.2 syntax with Transpec
    This conversion is done by Transpec 2.2.4 with the following command:
        transpec -f test/integration/default/serverspec/
    
    * 7 conversions
        from: it { should ... }
          to: it { is_expected.to ... }
    
    * 2 conversions
        from: its(:attr) { }
          to: describe '#attr' do subject { super().attr }; it { } end
    
    For more details: https://github.com/yujinakayama/transpec#supported-conversions

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -5,30 +5,36 @@ require 'spec_helper'
   libmagickwand-dev
 ).each do |pkg|
   describe package(pkg) do
-    it { should be_installed }
+    it { is_expected.to be_installed }
   end
 end
 
 describe user('deploy') do
-  it { should exist }
+  it { is_expected.to exist }
 end
 
 describe file('/etc/nginx/sites-enabled/socketproxy.conf') do
-  it { should be_linked_to '/etc/nginx/sites-available/socketproxy.conf' }
+  it { is_expected.to be_linked_to '/etc/nginx/sites-available/socketproxy.conf' }
 end
 
 describe file('/etc/nginx/sites-available/socketproxy.conf') do
-  its(:content) { should match(%r{root .*/landing_pages/.*}) }
+  describe '#content' do
+    subject { super().content }
+    it { is_expected.to match(%r{root .*/landing_pages/.*}) }
+  end
 end
 
 describe service('nginx') do
-  it { should be_enabled }
+  it { is_expected.to be_enabled }
 end
 
 describe process('nginx') do
-  its(:args) { should match(%r{master process /usr/sbin/nginx}) }
+  describe '#args' do
+    subject { super().args }
+    it { is_expected.to match(%r{master process /usr/sbin/nginx}) }
+  end
 end
 
 describe port(8080) do
-  it { should be_listening.with('tcp') }
+  it { is_expected.to be_listening.with('tcp') }
 end
