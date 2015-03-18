7bbdef7d74fde2cac213651f3d3e7d5889829353
Eddie Hurtig <eddie@hurtigtechnologies.com>
2015-03-18 14:53:03 -0400

    Bump to v1.2.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'devops@evertrue.com'
 license          'All rights reserved'
 description      'Installs/Configures et_landing_pages_app'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.1.1'
+version          '1.2.0'
 
 depends 'et_rails_app', '~> 8.0'
 depends 'et_users',     '>= 1.4.1'
