7335b93085ee9826e3391e6436d269d54d5116b7
Jeff Byrnes <thejeffbyrnes@gmail.com>
2015-12-15 14:08:12 -0500

    Bump to et_rails app ~> 10.0
    Changeset as of this writing:
    
    github.com/evertrue/et_rails_app-cookbook/compare/v8.0.2...v10.0.0

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,6 @@ description      'Installs/Configures et_landing_pages_app'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.2.0'
 
-depends 'et_rails_app', '~> 8.0'
+depends 'et_rails_app', '~> 10.0'
 depends 'et_users',     '>= 1.4.1'
 depends 'apt'
