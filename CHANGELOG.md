0eb114ab5fd7cd4dfd8af831830ae6ee7904a9d1
Jeff Byrnes <thejeffbyrnes@gmail.com>
2015-12-15 14:11:22 -0500

    Bump to v2.0.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # et_landing_pages_app CHANGELOG
 
+## v2.0.0 (2015-12-15)
+
+* Bump to `et_rails_app` ~> 10.0
+
 ## v1.2.0 (2015-03-18)
 
 * Update `et_rails_app` to ~> 8.0
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'devops@evertrue.com'
 license          'All rights reserved'
 description      'Installs/Configures et_landing_pages_app'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.2.0'
+version          '2.0.0'
 
 depends 'et_rails_app', '~> 10.0'
 depends 'et_users',     '>= 1.4.1'
